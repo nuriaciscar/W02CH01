2624c4009f2d1f3c2a4bd9b95d434dfc676cb5c3
nuriaciscar <nuriaciscar@hotmail.com>
2021-10-05 18:54:51 +0200

    Added all the conditions to testing

--- add.test.js
+++ add.test.js
@@ -1,9 +1,16 @@
 
 function strictEquals(a,b){
   let result= (Object.is(a, b));
-  return result;
+  
 
+  if(isNaN (a)|| isNaN(b)){
+   result = false;
+  }
+  
+return result;
 }
+
+
 
 
 describe("function strictEquals", function (){
@@ -20,6 +27,98 @@ test("When it receives a and b,it should return a === b", function(){
   expect(result).toBe(expected);
 
 });
+test("When it receives a and b,it should return a === b", function(){
+	//Arange
+	const a = NaN;
+	const b = NaN;
+	const expected = true; 
+	
+  //Act
+	const result= strictEquals(a,b);
+
+	//Assert
+  expect(result).toBe(expected);
+
 });
+test("When it receives a and b,it should return a === b", function(){
+	//Arange
+	const a = 0;
+	const b = -0;
+	const expected = true; 
+	
+  //Act
+	const result= strictEquals(a,b);
+
+	//Assert
+  expect(result).toBe(expected);
+
+});
+test("When it receives a and b,it should return a === b", function(){
+	//Arange
+	const a = -0;
+	const b = 0;
+	const expected = true; 
+	
+  //Act
+	const result= strictEquals(a,b);
+
+	//Assert
+  expect(result).toBe(expected);
+
+});
+test("When it receives a and b,it should return a === b", function(){
+	//Arange
+	const a = 1;
+	const b = "1";
+	const expected = true; 
+	
+  //Act
+	const result= strictEquals(a,b);
+
+	//Assert
+  expect(result).toBe(expected);
+
+});
+test("When it receives a and b,it should return a === b", function(){
+	//Arange
+	const a = true;
+	const b = false;
+	const expected = true; 
+	
+  //Act
+	const result= strictEquals(a,b);
+
+	//Assert
+  expect(result).toBe(expected);
+
+});
+test("When it receives a and b,it should return a === b", function(){
+	//Arange
+	const a = false;
+	const b = false;
+	const expected = true; 
+	
+  //Act
+	const result= strictEquals(a,b);
+
+	//Assert
+  expect(result).toBe(expected);
+
+});
+test("When it receives a and b,it should return a === b", function(){
+	//Arange
+	const a = "Water";
+	const b = "oil";
+	const expected = true; 
+	
+  //Act
+	const result= strictEquals(a,b);
+
+	//Assert
+  expect(result).toBe(expected);
+
+});
+});
+
 
 
